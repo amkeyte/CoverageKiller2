6e4b04b2b2a5ce941586d6bc87c3698944ddf898
Arryn <amkeyte@gmail.com>
2024-10-11 23:18:44 -0700

    Logging functions updated.

---
+++ src/CoverageKiller2/LoggingLoader.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using Serilog.Events;
+
+public class LoggingLoader
+{
+    private static ILogger _logger;
+    public static LogEventLevel Level { get; private set; }
+    // Method to configure Serilog
+    public static void Configure(string logFile, LogEventLevel logEventLevel)
+    {
+        Level = logEventLevel;
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Is(logEventLevel)
+            .WriteTo.Async(a => a.File(logFile)) // Log to a file asynchronously
+            .CreateLogger();
+    }
+
+    // Clean up method for the logger
+    public static void Cleanup()
+    {
+        Log.CloseAndFlush(); // Ensures that all log events are flushed before closing
+    }
+}
