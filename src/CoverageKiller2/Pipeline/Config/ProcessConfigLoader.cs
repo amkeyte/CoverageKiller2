768ca105bf4e92aec95310e0dd523e0a1c54d95c
Arryn <amkeyte@gmail.com>
2025-03-14 12:07:32 -0700

    Added ProcessorConfigLoader and config model

---
+++ src/CoverageKiller2/Pipeline/Config/ProcessConfigLoader.cs
@@ -0,0 +1,50 @@
+﻿using CoverageKiller2.Logging;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CoverageKiller2.Pipeline.Config
+{
+    public class ProcessorConfigLoader
+    {
+        public ProcessorConfig ProcessorConfig { get; private set; }
+        public Tracer Tracer { get; } = new Tracer(typeof(ProcessorConfig));
+
+        public bool LoadConfig(string xmlFilePath)
+        {
+            if (string.IsNullOrEmpty(xmlFilePath))
+            {
+                Tracer.Log("Error: XML file path is null or empty.");
+                return false;
+            }
+
+            if (!File.Exists(xmlFilePath))
+            {
+                Tracer.Log($"Error: File not found at {xmlFilePath}");
+                return false;
+            }
+
+            FileStream fileStream = null;
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(ProcessorConfig));
+                fileStream = new FileStream(xmlFilePath, FileMode.Open, FileAccess.Read);
+                ProcessorConfig = (ProcessorConfig)serializer.Deserialize(fileStream);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Tracer.Log($"Error loading XML: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                if (fileStream != null)
+                {
+                    fileStream.Close();
+                    fileStream.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ src/CoverageKiller2/Pipeline/Config/ProcessorConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace CoverageKiller2.Pipeline.Config
+{
+    [XmlRoot("ProcessorConfig")]
+    public class ProcessorConfig
+    {
+        [XmlAttribute("Name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("Description")]
+        public string Description { get; set; }
+
+        [XmlAttribute("SourceTemplate")]
+        public string SourceTemplate { get; set; }
+
+        [XmlElement("PipelineConfig")]
+        public PipelineConfig PipelineConfig { get; set; }
+    }
+
+    public class PipelineConfig
+    {
+        [XmlElement("Steps")]
+        public Steps Steps { get; set; }
+    }
+
+    public class Steps
+    {
+        [XmlElement("Step")]
+        public List<Step> StepList { get; set; } = new List<Step>();
+    }
+
+    public class Step
+    {
+        [XmlAttribute("Name")]
+        public string Name { get; set; }
+    }
+}
