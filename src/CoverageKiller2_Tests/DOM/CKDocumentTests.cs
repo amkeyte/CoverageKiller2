cb04cdf8769ed4fa8a659d6fbe03121654f1a14a
Arryn <amkeyte@gmail.com>
2025-03-21 07:48:05 -0700

    Unit Test project added for DOM

---
+++ src/CoverageKiller2_Tests/DOM/CKDocumentTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoverageKiller2.DOM.Tests
+{
+    [TestClass()]
+    public class CKDocumentTests
+    {
+        [TestMethod()]
+        public void CKDocumentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CKDocumentTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ActivateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CloseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteSectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFooterRangeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetHeaderRangeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CopyFooterToTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CopyHeaderToTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CopyHeaderAndFooterToTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
