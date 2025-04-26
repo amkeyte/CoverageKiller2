a16da00deb56305825496858008a658647b9d56a
Arryn <amkeyte@gmail.com>
2025-04-26 14:05:01 -0700

    20250426 0026 fixed in LH.

--- src/CoverageKiller2/Logging/LH.cs
+++ src/CoverageKiller2/Logging/LH.cs
@@ -49,13 +49,10 @@ namespace CoverageKiller2.Logging
         {
             if (table == null || string.IsNullOrWhiteSpace(markerText)) return null;
 
-            var doc = table.Document;
             //juts try to grab the few paragraphs right before the table. title tag should be there.
-            var searchRangeStart = Math.Max(1, table.Start - 100);
+            //Bug 20250426 00026: fixed - set max from 1 to 0; preents table.start = 0 -> out of range
+            var searchRangeStart = Math.Max(0, table.Start - 100);
             var paraList = table.Document.Range(searchRangeStart, table.Start).Paragraphs;
-
-
-
 
             int start = table.COMRange.Start;
 
